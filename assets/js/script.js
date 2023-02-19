2ab29e3a8f9206bcd1b70512df9ab4ba8fdad7c2
dilsoncampelo10 <dilsono316@gmail.com>
2023-02-19 15:14:19 -0300

    listando pokemons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const limit = 5;
+const limit = 10;
 const offset = 0;
 const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
 
@@ -6,14 +6,14 @@ const pokemonList = document.querySelector('#pokemonList');
 
 let createPokemon = (pokemon) =>{
     return `<li class="pokemon">
-                <span class="number">#001</span>
+                <span class="number">#${pokemon.order}</span>
                 <h2 class="name">${pokemon.name}</h2>
                 <div class="content">
                     <div class="types">
-                        <span>Grass</span>
-                        <span>Poison</span>
+                        <span>${pokemon.types[0].type.name}</span>
+                        
                     </div>
-                    <div><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"></div>
+                    <div><img src="${pokemon.sprites.other.home.front_default}" alt="Bulbasaur"></div>
                 </div>
             </li>`
     
@@ -24,11 +24,15 @@ let a = fetch(url)
     .then((json)=> json.results)
     .then((pokemon)=>{
         pokemon.map((pokemons)=>{
-            
-            let htmlPokemon = createPokemon(pokemon)
-            pokemonList.innerHTML += htmlPokemon;
+            fetch(pokemons.url).then((details)=> details.json())
+            .then((jsonPoke)=> {
+                let htmlPokemon = createPokemon(jsonPoke);
+                pokemonList.innerHTML += htmlPokemon;
+                console.log(jsonPoke.types[0].type.name)
+               
+            })
+           
         })
-
         
 
     })
