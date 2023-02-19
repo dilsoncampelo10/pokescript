108dcbfbee26037ec499f8f7d5a227d4ce810b59
dilsoncampelo10 <dilsono316@gmail.com>
2023-02-19 15:23:13 -0300

    encapsulamento

--- assets/js/Pokemon.js
+++ assets/js/Pokemon.js
@@ -4,17 +4,29 @@ class Pokemon {
     _types = [];
     _type;
 
-    setId(){
-
+    setId(id){
+        this._id = id;
     }
     getId(){
-
+        return this._id;
     }
-    setName(){
-
+    setName(name){
+        this._name = name;
     }
     getName(){
-
+        return this._name;
+    }
+    setTypes(types=[]){
+        this._types = types;
+    }
+    getTypes(){
+        return this._types;
+    }
+    setType(type){
+        this._type = type;
+    }
+    getType(){
+        return this._type;
     }
    
 }
